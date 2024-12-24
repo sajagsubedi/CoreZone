a0ef10ada3865d406fb3d9f6742a093fa8c8b414
Sajag Subedi <sajagsubedi03@gmail.com>
2024-12-24 21:01:32 +0545

    Add quote section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const crsr = document.querySelector("#cursor");
 const navLinks = document.querySelectorAll("#nav #menu li a");
+let mm = gsap.matchMedia();
 
 document.addEventListener("mousemove", function (e) {
   crsr.style.display = "block";
@@ -39,7 +40,55 @@ gsap.to("#main", {
     end: "top -70%",
   },
 });
+// for desktop
+mm.add("(min-width: 800px)", () => {
+  gsap.to("#leftQuote", {
+    x: 70,
+    y: 20,
+    scrollTrigger: {
+      trigger: "#leftQuote",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 100%",
+      scrub: 2,
+    },
+  });
+  gsap.to("#rightQuote", {
+    x: -70,
+    y: -20,
+    scrollTrigger: {
+      trigger: "#leftQuote",
+      scroller: "body",
+      start: "top 90%",
+      end: "top 100%",
+      scrub: 2,
+    },
+  });
+});
 
+// for phone
+mm.add("(max-width: 800px)", () => {
+  gsap.to("#leftQuote", {
+    opacity: 1,
+    scrollTrigger: {
+      trigger: "#leftQuote",
+      scroller: "body",
+      start: "top 70%",
+      end: "top 90%",
+      scrub: 2,
+    },
+  });
+  gsap.to("#rightQuote", {
+    opacity: 1,
+    scrollTrigger: {
+      trigger: "#leftQuote",
+      scroller: "body",
+      start: "top 70%",
+      end: "top 90%",
+      scrub: 2,
+    },
+  });
+});
 const menuToggle = document.getElementById("menuToggle");
 const nav = document.getElementById("nav");
 menuToggle.addEventListener("click", () => {
